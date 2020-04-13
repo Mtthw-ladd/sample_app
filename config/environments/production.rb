679d2739c9efb6825d01ea78543bf3557105df7b
Matthew Ladd <mtthwladd@gmail.com>
2020-04-13 23:03:00 +0000

    updating port number

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '2525',
+    :port           => '25',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
