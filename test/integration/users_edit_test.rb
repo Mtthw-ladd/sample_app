1153ffb0112d912cc533107b985b8d09f900477f
Matthew Ladd <mtthwladd@gmail.com>
2020-04-14 18:56:25 +0000

    Making updates to the production

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,7 +32,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+    patch user_path(@user), params: { user: { name: name, 
+                                              email: email, 
+                                              password: "", 
+                                              password_confirmation: "" } }
     assert_not flash.empty? 
     assert_redirected_to @user
     @user.reload
@@ -68,6 +71,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
   
-  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 
 end
